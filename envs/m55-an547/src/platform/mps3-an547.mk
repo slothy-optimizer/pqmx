9e31e8f8f400c0bd5e6c042442836bad5820d732
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-05-08 14:26:03 +0800

    add an547 tests and fix them

--- envs/m55-an547/src/platform/mps3-an547.mk
+++ envs/m55-an547/src/platform/mps3-an547.mk
@@ -16,33 +16,11 @@ CPPFLAGS += \
 	--sysroot=$(SYSROOT) \
 	-DARMCM55
 
-ARCH_FLAGS += \
-	-mcpu=cortex-m55 \
-	-mthumb \
-	-mfloat-abi=hard -mfpu=fpv4-sp-d16 \
-
-CPPFLAGS += \
-	-Iplatform/
-
-CFLAGS += \
-	$(ARCH_FLAGS) \
-	--specs=nosys.specs
-
-LDSCRIPT = platform/mps3.ld
-
-LDFLAGS += \
-	--specs=nosys.specs \
-	-Wl,--wrap=_write \
-	-Wl,--wrap=_read \
-	-ffreestanding \
-	-T$(LDSCRIPT) \
-	$(ARCH_FLAGS)
-
 HAL_SRC += \
-	platform/startup_ARMCM55.c \
-	platform/system_ARMCM55.c \
-	platform/semihosting.c \
-	platform/uart.c
+	src/platform/startup_ARMCM55.c \
+	src/platform/system_ARMCM55.c \
+	src/platform/semihosting.c \
+	src/platform/uart.c
 HAL_OBJ = $(call objs,$(HAL_SRC))
 
 OBJ += $(HAL_OBJ)
