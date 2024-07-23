56afc76bd03968536d8851b5f77bcd47011f3631
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2024-07-23 12:00:14 +0800

    add nix configuration

---
+++ flake.nix
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: Apache-2.0
+
+{
+  description = "pqmx";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            packages = builtins.attrValues {
+              inherit (pkgs)
+                gcc-arm-embedded-13
+                direnv
+                nix-direnv
+                openocd
+                qemu
+                ;
+            };
+          };
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
