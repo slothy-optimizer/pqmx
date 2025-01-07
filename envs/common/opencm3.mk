ce99eb8517622f56f1055280b9dde140e610f284
Amin Abdulrahman <amin@abdulrahman.de>
2025-01-07 11:02:04 +0100

    Pass device name to the build * Reason: When printing benchmark results, it is practical to automatically add the name of the target the benchmark was run on. This eases further processing.

--- envs/common/opencm3.mk
+++ envs/common/opencm3.mk
@@ -76,7 +76,7 @@ OBJECTS = $(OBJECTS_C) $(OBJECTS_ASM)
 
 $(OBJECTS_C): $(BUILD_DIR)/%.o: %
 	mkdir -p $(@D)
-	$(CC) $(CFLAGS) -c -o $@ $<
+	$(CC) $(CFLAGS) -DDEVICE=\"$(DEVICE)\" -c -o $@ $<
 
 $(OBJECTS_ASM): $(BUILD_DIR)/%.o: %
 	mkdir -p $(@D)
