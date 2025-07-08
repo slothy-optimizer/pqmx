1dfb24a18becffabd6a1cd2161f151eeb20e944c
willieyz <willie.zhao@chelpis.com>
2025-07-08 18:41:36 +0800

    wip: increase the stack size
    Signed-off-by: willieyz <willie.zhao@chelpis.com>

--- envs/ek-ra8m1/src/RA8M1/ra_cfg/fsp_cfg/bsp/bsp_cfg.h
+++ envs/ek-ra8m1/src/RA8M1/ra_cfg/fsp_cfg/bsp/bsp_cfg.h
@@ -26,7 +26,7 @@
              #define BSP_CFG_BOOT_IMAGE (1)
             #endif
             #define BSP_CFG_MCU_VCC_MV (3300)
-            #define BSP_CFG_STACK_MAIN_BYTES (0xA0000) // F000 -> A0000
+            #define BSP_CFG_STACK_MAIN_BYTES (0xC0000) // F000 -> A0000 -> C0000
             #define BSP_CFG_HEAP_BYTES (0)
             #define BSP_CFG_PARAM_CHECKING_ENABLE (0)
             #define BSP_CFG_ASSERT (0)
