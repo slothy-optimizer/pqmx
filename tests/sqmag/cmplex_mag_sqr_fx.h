f7e24279a396c629f7d117137ee6dcc05eb5a1ce
willieyz <willie.zhao@chelpis.com>
2025-07-25 18:49:28 +0800

    Adjust sqmag test to match file and function names in updated SLOTHY
    Signed-off-by: willieyz <willie.zhao@chelpis.com>

--- tests/sqmag/cmplex_mag_sqr_fx.h
+++ tests/sqmag/cmplex_mag_sqr_fx.h
@@ -2,10 +2,10 @@
 #define CPX_MAG_SQR_FX_H
 
 void cmplx_mag_sqr_fx          ( uint64_t *out, uint64_t *in, size_t sz );
-void cmplx_mag_sqr_fx_opt_M55_unroll1(uint64_t *, uint64_t *, size_t);
-void cmplx_mag_sqr_fx_opt_M55_unroll2(uint64_t *, uint64_t *, size_t);
-void cmplx_mag_sqr_fx_opt_M55_unroll4(uint64_t *, uint64_t *, size_t);
-void cmplx_mag_sqr_fx_opt_M85_unroll1(uint64_t *, uint64_t *, size_t);
-void cmplx_mag_sqr_fx_opt_M85_unroll2(uint64_t *, uint64_t *, size_t);
-void cmplx_mag_sqr_fx_opt_M85_unroll4(uint64_t *, uint64_t *, size_t);
+void cmplx_mag_sqr_fx_unroll1_opt_M55(uint64_t *, uint64_t *, size_t);
+void cmplx_mag_sqr_fx_unroll2_opt_M55(uint64_t *, uint64_t *, size_t);
+void cmplx_mag_sqr_fx_unroll4_opt_M55(uint64_t *, uint64_t *, size_t);
+void cmplx_mag_sqr_fx_unroll1_opt_M85(uint64_t *, uint64_t *, size_t);
+void cmplx_mag_sqr_fx_unroll2_opt_M85(uint64_t *, uint64_t *, size_t);
+void cmplx_mag_sqr_fx_unroll4_opt_M85(uint64_t *, uint64_t *, size_t);
 #endif
