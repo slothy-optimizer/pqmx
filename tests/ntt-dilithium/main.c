e6e79d45df09a2c28a4c22d376fed8dd5691aeda
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-08-25 09:42:46 +0800

    MVE iNTT: add intt test for Dilithium

--- tests/ntt-dilithium/main.c
+++ tests/ntt-dilithium/main.c
@@ -48,7 +48,7 @@
 
 #include "ntt_ref.h"
 
-// base
+// base NTT
 void ntt_dilithium_12_34_56_78(int32_t *src);
 void ntt_dilithium_12_34_56_78_no_trans_vld4(int32_t *src);
 void ntt_dilithium_123_456_78(int32_t *src);
@@ -62,6 +62,15 @@ void ntt_dilithium_123_456_78_opt_size_m55(int32_t *src);
 void ntt_dilithium_12_34_56_78_opt_m85(int32_t *src);
 void ntt_dilithium_12_34_56_78_no_trans_vld4_opt_m85(int32_t *src);
 void ntt_dilithium_123_456_78_opt_size_m85(int32_t *src);
+
+// base iNTT
+void intt_dilithium_12_34_56_78(int32_t *src);
+
+// M55 iNTT
+void intt_dilithium_12_34_56_78_opt_m55(int32_t *src);
+
+// M85 iNTT
+void intt_dilithium_12_34_56_78_opt_m85(int32_t *src);
 
 #define NTT_LAYERS             8
 #define NTT_SIZE               (1u << NTT_LAYERS)
@@ -127,7 +136,7 @@ int test_ntt_ ## var ()                                                 \
     return( 0 );                                                        \
 }
 
-// base
+// base NTT
 MAKE_TEST_FWD(l2222,ntt_dilithium_12_34_56_78,1)
 MAKE_TEST_FWD(l2222_no_trans_vld4,ntt_dilithium_12_34_56_78_no_trans_vld4,1)
 MAKE_TEST_FWD(l332,ntt_dilithium_123_456_78,1)
@@ -139,6 +148,59 @@ MAKE_TEST_FWD(l332_opt_size_m55,ntt_dilithium_123_456_78_opt_size_m55,1)
 MAKE_TEST_FWD(l2222_opt_m85,ntt_dilithium_12_34_56_78_opt_m85,1)
 MAKE_TEST_FWD(l2222_no_trans_vld4_opt_m85,ntt_dilithium_12_34_56_78_no_trans_vld4_opt_m85,1)
 MAKE_TEST_FWD(l332_opt_size_m85,ntt_dilithium_123_456_78_opt_size_m85,1)
+
+// Define macro for iNTT tests
+#define MAKE_TEST_INV(var,func,rev4)                                    \
+int test_intt_ ## var ()                                                \
+{                                                                       \
+    debug_test_start( "iNTT s32 for " #func );                          \
+    int32_t src[NTT_SIZE]      __attribute__((aligned(16)));            \
+    int32_t src_copy[NTT_SIZE] __attribute__((aligned(16)));            \
+                                                                        \
+    /* Setup input */                                                   \
+    fill_random_u32( (uint32_t*) src, NTT_SIZE );                       \
+    mod_reduce_buf_s32( src, NTT_SIZE, modulus );                       \
+                                                                        \
+    /* Step 1: Reference iNTT */                                        \
+    memcpy( src_copy, src, sizeof( src ) );                             \
+    invntt_tomont_ref( src_copy );                                      \
+    mod_reduce_buf_s32( src_copy, NTT_SIZE, modulus );                  \
+                                                                        \
+    /* Step 2: MVE-based iNTT - expects input in transposed format */   \
+    if(rev4) buf_bitrev_4( src );  /* Apply transposition to input */   \
+    measure_start();                                                    \
+    (func)( src );                                                      \
+    measure_end();                                                      \
+                                                                        \
+    mod_reduce_buf_s32( src, NTT_SIZE, modulus );                       \
+                                                                        \
+    /* TODO: Add scaling to inv NTT asm                                 \
+     * The assembly iNTT should scale by 2^(-8) * 2^32 like the ref     \
+     */                                                                 \
+    const int32_t correction_factor = 16382; /* pow(4186116, Q-2, Q) */ \
+    for (unsigned i = 0; i < NTT_SIZE; i++) {                           \
+        src[i] = mod_mul_s32(src[i], correction_factor, modulus);       \
+    }                                                                   \
+                                                                        \
+    if( compare_buf_u32( (uint32_t const*) src, (uint32_t const*) src_copy, \
+                         NTT_SIZE ) != 0 )                              \
+    {                                                                   \
+        debug_print_buf_s32( src_copy, NTT_SIZE, "Reference" );         \
+        debug_print_buf_s32( src, NTT_SIZE, "MVE" );                    \
+        debug_test_fail();                                              \
+        return( 1 );                                                    \
+    }                                                                   \
+    debug_test_ok();                                                    \
+                                                                        \
+    return( 0 );                                                        \
+}
+
+// base iNTT
+MAKE_TEST_INV(l2222,intt_dilithium_12_34_56_78,1)
+// M55 iNTT
+MAKE_TEST_INV(l2222_opt_m55,intt_dilithium_12_34_56_78_opt_m55,1)
+// M85 iNTT
+MAKE_TEST_INV(l2222_opt_m85,intt_dilithium_12_34_56_78_opt_m85,1)
 
 uint64_t hal_get_time();
 
@@ -173,7 +235,7 @@ int bench_ ## var ()                                           \
     hal_pmu_send_stats_wrapper(&stats);                        \
     return( 0 );                                               \
 }
-// base
+// base NTT
 MAKE_BENCH(ntt_l2222,ntt_dilithium_12_34_56_78)
 MAKE_BENCH(ntt_l2222_no_trans_vld4,ntt_dilithium_12_34_56_78_no_trans_vld4)
 MAKE_BENCH(ntt_l332,ntt_dilithium_123_456_78)
@@ -185,14 +247,20 @@ MAKE_BENCH(ntt_l332_opt_size_m55,ntt_dilithium_123_456_78_opt_size_m55)
 MAKE_BENCH(ntt_l2222_opt_m85,ntt_dilithium_12_34_56_78_opt_m85)
 MAKE_BENCH(ntt_l2222_no_trans_vld4_opt_m85,ntt_dilithium_12_34_56_78_no_trans_vld4_opt_m85)
 MAKE_BENCH(ntt_l332_opt_size_m85,ntt_dilithium_123_456_78_opt_size_m85)
+// base iNTT
+MAKE_BENCH(intt_l2222,intt_dilithium_12_34_56_78)
+// M55 iNTT
+MAKE_BENCH(intt_l2222_opt_m55,intt_dilithium_12_34_56_78_opt_m55)
+// M85 iNTT
+MAKE_BENCH(intt_l2222_opt_m85,intt_dilithium_12_34_56_78_opt_m85)
 
 int main(void)
 {
     int ret = 0;
 
-    debug_printf( "\nDilithium NTT Test!\n" );
+    debug_printf( "\nDilithium NTT/iNTT Test!\n" );
 
-    // base
+    // base NTT
     ret |= test_ntt_l2222();
     if( ret != 0 )
         return( 1 );
@@ -202,7 +270,7 @@ int main(void)
     ret |= test_ntt_l332();
     if( ret != 0 )
         return( 1 );
-    // M55
+    // M55 NTT
     ret |= test_ntt_l2222_opt_m55();
     if( ret != 0 )
         return( 1 );
@@ -212,7 +280,7 @@ int main(void)
     ret |= test_ntt_l332_opt_size_m55();
     if( ret != 0 )
         return( 1 );
-    // M85
+    // M85 NTT
     ret |= test_ntt_l2222_opt_m85();
     if( ret != 0 )
         return( 1 );
@@ -222,21 +290,41 @@ int main(void)
     ret |= test_ntt_l332_opt_size_m85();
     if( ret != 0 )
         return( 1 );
+    
+    // base iNTT
+    ret |= test_intt_l2222();
+    if( ret != 0 )
+        return( 1 );
+    // M55 iNTT
+    ret |= test_intt_l2222_opt_m55();
+    if( ret != 0 )
+        return( 1 );
+    // M85 iNTT
+    ret |= test_intt_l2222_opt_m85();
+    if( ret != 0 )
+        return( 1 );
 
     hal_pmu_enable();
-    debug_printf( "Dilithium NTT Bench!\n" );
-    // base
+    debug_printf( "Dilithium NTT/iNTT Bench!\n" );
+    // base NTT
     bench_ntt_l2222();
     bench_ntt_l2222_no_trans_vld4();
     bench_ntt_l332();
-    // M55
+    // M55 NTT
     bench_ntt_l2222_opt_m55();
     bench_ntt_l2222_no_trans_vld4_opt_m55();
     bench_ntt_l332_opt_size_m55();
-    // M85
+    // M85 NTT
     bench_ntt_l2222_opt_m85();
     bench_ntt_l2222_no_trans_vld4_opt_m85();
     bench_ntt_l332_opt_size_m85();
+    
+    // base iNTT
+    bench_intt_l2222();
+    // M55 iNTT  
+    bench_intt_l2222_opt_m55();
+    // M85 iNTT
+    bench_intt_l2222_opt_m85();
 
     debug_printf( "Done!\n" );
     hal_pmu_disable();
